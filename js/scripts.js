5e24d235811beac1b1d6e6d0833330a06d930e44
Lykov_Ilya <ilya.lykov.program@gmail.com>
2022-11-11 08:06:03 +0400

    update: work with scripts; try to add some functionality, but i cant

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,38 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
+    const tabContent = document.querySelectorAll(".tabcontent"),
+        tabHeaderItems = document.querySelector(".tabheader__items"),
+        tabHeaderItem = tabHeaderItems.querySelectorAll(".tabheader__item");
+    hideTabContent();
 
+    tabHeaderItem.forEach((tab) => {
+        tab.classList.remove("tabheader__item_active");
+    });
+    tabHeaderItems.addEventListener("click", (event) => {
+        event.preventDefault();
+        if (event.target && event.target.classList.contains("tabheader__item")) {
+            showTabContent(returnIndexOfTabHeaderItem(event.target));
+            event.target.classList.toggle("tabheader__item_active");
+        }
+    });
+
+    function showTabContent(i = 0) {
+        console.log("show");
+        tabContent[i].classList.remove("hide");
+        tabContent[i].classList.add("show");
+    }
+    function hideTabContent() {
+        tabContent.forEach((tab) => {
+            tab.classList.add("hide");
+        });
+    }
+    function returnIndexOfTabHeaderItem(item) {
+        tabHeaderItem.forEach((tab, index) => {
+            if (item === tab) {
+                console.log(item === tab);
+                return index;
+            }
+        });
+    }
 });
