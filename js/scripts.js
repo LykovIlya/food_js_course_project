c2e89e7f5e817397e07593420039be419aba8585
Lykov Ilya <ilya.lykov.program@gmail.com>
2022-11-14 14:28:09 +0400

    update: i added some js in timer block

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
+    //tabs
+
     const tabsContent = document.querySelectorAll(".tabcontent"),
         tabHeaderItemsWrapper = document.querySelector(".tabheader__items"),
         tabHeaderItems = tabHeaderItemsWrapper.querySelectorAll(".tabheader__item");
@@ -10,15 +12,13 @@ document.addEventListener("DOMContentLoaded", () => {
     hideTabContent();
     showTabContent();
 
-    console.log(Array.from(tabHeaderItems).findIndex(value => value.innerText.toLowerCase() === "сбалансированное"));
-    console.log([...tabHeaderItems].findIndex(value => value.innerText.toLowerCase() === "сбалансированное"));
 
 
     tabHeaderItemsWrapper.addEventListener("click", (event) => {
         event.preventDefault();
         const target = event.target;
         if (target && target.classList.contains("tabheader__item")) {
-            let i = [...tabHeaderItems].findIndex(value => value.innerText === target.innerText);
+            let i = [...tabHeaderItems].findIndex((value => value.innerText === target.innerText));
             hideTabContent();
             showTabContent(i);
         }
@@ -41,4 +41,43 @@ document.addEventListener("DOMContentLoaded", () => {
         tabsContent[i].classList.add("fade");
         tabHeaderItems[i].classList.add("tabheader__item_active");
     }
+
+    //timer
+
+    const deadLine = ("2022-11-19");
+    getCountdownTimer(deadLine);
+
+    function getCountdownTimer() {
+        const timeToEnd = Date.parse(deadLine) - Date.parse(new Date()),
+            days = Math.floor((timeToEnd) / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((timeToEnd) / (1000 * 60 * 60) % 24),
+            minutes = Math.floor((timeToEnd) / (1000 * 60) % 60),
+            seconds = Math.floor((timeToEnd) / (1000) % 60);
+
+        return {
+            "days": days,
+            "hours": hours,
+            "minutes": minutes,
+            "seconds": seconds
+        };
+
+
+        console.log(`${days}:${hours}:${minutes}:${seconds}`);
+
+    }
+
+    function showTimer(selector, timeToEndObject) {
+        const days = selector.querySelector("#days"),
+            hours = selector.querySelector("#hours"),
+            minutes = selector.querySelector("#minutes"),
+            seconds = selector.querySelector("#seconds");
+
+        days.innerHTML = timeToEndObject.days;
+        hours.innerHTML = timeToEndObject.hours;
+        minutes.innerHTML = timeToEndObject.minutes;
+        seconds.innerHTML = timeToEndObject.seconds;
+
+
+    }
+
 });
